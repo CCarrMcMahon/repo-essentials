215aab2750f147b23c542a285dea6c9bda6ed376
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-03 22:33:37 -0600

    Add initial project files for RepoEssentials plugin

---
+++ Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+
+namespace RepoEssentials;
+
+public static class RepoEssentialsInfo
+{
+    public const string REPO_NAME = "repo-essentials";
+    public const string PROJECT_NAME = "RepoEssentials";
+    public const string PLUGIN_NAME = "R.E.P.O Essentials";
+    public const string PLUGIN_GUID = "org.ccarrmcmahon.plugins.repo.repoessentials";
+    public const string PLUGIN_VERSION = "1.0.0";
+}
+
+
+[BepInPlugin(RepoEssentialsInfo.PLUGIN_GUID, RepoEssentialsInfo.PLUGIN_NAME, RepoEssentialsInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    internal static new ManualLogSource Logger;
+
+    private void Awake()
+    {
+        // Plugin startup logic
+        Logger = base.Logger;
+        Logger.LogInfo($"Plugin {RepoEssentialsInfo.PLUGIN_GUID} is loaded!");
+    }
+}
