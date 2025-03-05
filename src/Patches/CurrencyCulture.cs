eeba82b521c0b5ef56fde469190904c85f829ff6
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-05 01:53:14 -0600

    #3: Switch back to annotated class patches for currency culture

--- src/Patches/CurrencyCulture.cs
+++ src/Patches/CurrencyCulture.cs
@@ -6,9 +6,8 @@ namespace RepoEssentials.src.patches;
 
 public static class CurrentCulturePatch {
     public static void SemiFuncDollarGetStringPatch(Harmony harmony) {
-        MethodInfo semiFuncDollarGetString = AccessTools.Method(typeof(SemiFunc), nameof(SemiFunc.DollarGetString));
-        MethodInfo semiFuncDollarGetStringPatch = AccessTools.Method(typeof(SemiFunc_DollarGetString_Patch), "Prefix");
-        harmony.Patch(semiFuncDollarGetString, prefix: new HarmonyMethod(semiFuncDollarGetStringPatch));
+        harmony.CreateClassProcessor(typeof(SemiFunc_DollarGetString_Patch)).Patch();
+        Plugin.Logger.LogInfo("SemiFunc.DollarGetString patch applied.");
     }
 
     public static void ApplyPatches(Harmony harmony) {
@@ -16,6 +15,7 @@ public static class CurrentCulturePatch {
     }
 }
 
+[HarmonyPatch(typeof(SemiFunc), nameof(SemiFunc.DollarGetString))]
 public class SemiFunc_DollarGetString_Patch {
     private static bool Prefix(int value, ref string __result) {
         __result = string.Format(CultureInfo.CurrentCulture, "{0:#,0}", value);
