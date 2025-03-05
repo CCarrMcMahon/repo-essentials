522c2288d52411e0f43b12ad415d737878701950
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-04 20:04:41 -0600

    #1: Refactor namespaces for better organization and clarity in GameInfo, Plugin, and PluginInfo classes

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -2,26 +2,25 @@
 using BepInEx.Logging;
 using HarmonyLib;
 
-namespace RepoEssentials;
+namespace RepoEssentials.src {
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
+    [BepInProcess(GameInfo.EXECUTABLE_NAME)]
+    public class Plugin : BaseUnityPlugin {
+        internal static new ManualLogSource Logger;
 
+        private void Awake() {
+            // Plugin startup logic
+            Logger = base.Logger;
+            Logger.LogInfo("Loading plugin...");
 
-[BepInPlugin(PluginInfo.GUID, PluginInfo.NAME, PluginInfo.VERSION)]
-[BepInProcess(GameInfo.EXECUTABLE_NAME)]
-public class Plugin : BaseUnityPlugin {
-    internal static new ManualLogSource Logger;
+            // Configure Harmony
+            Harmony harmony = new(PluginInfo.GUID);
 
-    private void Awake() {
-        // Plugin startup logic
-        Logger = base.Logger;
-        Logger.LogInfo("Loading plugin...");
+            // Apply patches
+            harmony.PatchAll();
+            Logger.LogInfo("Harmony patches are loaded!");
 
-        // Configure Harmony
-        Harmony harmony = new(PluginInfo.GUID);
-
-        // Apply patches
-        harmony.PatchAll();
-        Logger.LogInfo("Harmony patches are loaded!");
-
-        Logger.LogInfo("Plugin loaded successfully!");
+            Logger.LogInfo("Plugin loaded successfully!");
+        }
     }
 }
--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -1,11 +1,11 @@
-namespace RepoEssentials;
-
-public static class PluginInfo {
-    public const string GUID = "org.ccarrmcmahon.plugins.repo.essentials";
-    public const string NAME = "Essentials";
-    public const string VERSION = "0.1.0";
-    public const string AUTHOR = "CCarrMcMahon";
-    public const string ENTRY = "RepoEssentials.Plugin.Awake";
-    public const string MANIFEST = "manifest.json";
-    public const string ASSEMBLY = "Essentials.dll";
+namespace RepoEssentials.src {
+    public static class PluginInfo {
+        public const string GUID = "org.ccarrmcmahon.plugins.repo.essentials";
+        public const string NAME = "Essentials";
+        public const string VERSION = "0.1.0";
+        public const string AUTHOR = "CCarrMcMahon";
+        public const string ENTRY = "RepoEssentials.Plugin.Awake";
+        public const string MANIFEST = "manifest.json";
+        public const string ASSEMBLY = "Essentials.dll";
+    }
 }
