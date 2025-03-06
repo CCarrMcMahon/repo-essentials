d8507e86de7bb5e6c45d13e4cdff5895741d94ac
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-05 23:44:13 -0600

    #3: Bump version to 0.2.0 in project files and manifest

--- src/PluginInfo.cs
+++ src/PluginInfo.cs
@@ -4,7 +4,7 @@ namespace RepoEssentials.src;
 public static class PluginInfo {
     public const string GUID = "org.ccarrmcmahon.plugins.repo.essentials";
     public const string NAME = "Essentials";
-    public const string VERSION = "0.1.0";
+    public const string VERSION = "0.2.0";
     public const string AUTHOR = "CCarrMcMahon";
     public const string ENTRY = "RepoEssentials.Plugin.Awake";
     public const string MANIFEST = "manifest.json";
