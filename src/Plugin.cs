f49a70b01ae8e5f3ab4913aaa0e615195c04d56e
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-07 03:27:10 -0600

    #5: Add incompatibility attribute for NoLimitChatBox plugin

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -7,6 +7,7 @@ namespace RepoEssentials.src;
 
 [BepInPlugin(configs.PluginInfo.GUID, configs.PluginInfo.NAME, configs.PluginInfo.VERSION)]
 [BepInProcess(configs.GameInfo.EXECUTABLE_NAME)]
+[BepInIncompatibility("nickklmao.nolimitchatbox")]
 public class Plugin : BaseUnityPlugin {
     internal static new ManualLogSource Logger;
 
