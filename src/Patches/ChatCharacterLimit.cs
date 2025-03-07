c943dad34f003babe95e14e5b330f15aee325a82
Christopher McMahon <CCarrMcMahon@gmail.com>
2025-03-06 23:58:41 -0600

    #5: Implement chat character limit patching and refactor logging for clarity

--- src/Patches/ChatCharacterLimit.cs
+++ src/Patches/ChatCharacterLimit.cs
@@ -1,5 +1,8 @@
 using BepInEx.Configuration;
 using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
 
 namespace RepoEssentials.src.patches;
 
@@ -11,23 +14,29 @@ public static class ChatCharacterLimit {
         CharacterLimit = config.Bind("Chat", "CharacterLimit", 50, "The maximum number of characters allowed in a chat messages.");
     }
 
-    private static bool ChatManagerAwakePatch(Harmony harmony) {
+    private static void ChatManagerAwakePatch(Harmony harmony) {
         harmony.CreateClassProcessor(typeof(ChatManager_Awake_Patch)).Patch();
-        bool wasApplied = ChatManager_Awake_Patch.PatchSuccessful;
-        Plugin.Logger.LogDebug($"  - ChatManager_Awake_Patch: {wasApplied}");
+        Plugin.Logger.LogDebug($"  - ChatManager_Awake_Patch: True");
+    }
+
+    private static bool ChatManagerStateActivePatch(Harmony harmony) {
+        harmony.CreateClassProcessor(typeof(ChatManager_StateActive_Patch)).Patch();
+        bool wasApplied = ChatManager_StateActive_Patch.PatchSuccessful;
+        Plugin.Logger.LogDebug($"  - ChatManager_StateActive_Patch: {wasApplied}");
         return wasApplied;
     }
 
     private static bool ApplyPatches(Harmony harmony) {
         bool allApplied = true;
-        allApplied &= ChatManagerAwakePatch(harmony);
+        ChatManagerAwakePatch(harmony);
+        allApplied &= ChatManagerStateActivePatch(harmony);
         return allApplied;
     }
 
     public static void Initialize(ConfigFile config, Harmony harmony) {
         Plugin.Logger.LogDebug("Loading ChatCharacterLimit config...");
         LoadConfig(config);
-        Plugin.Logger.LogDebug("ChatCharacterLimit config loaded!");
+        Plugin.Logger.LogDebug("  > Success: True");
 
         Plugin.Logger.LogDebug("Applying ChatCharacterLimit patches...");
         bool patchLoaded = ApplyPatches(harmony);
@@ -38,12 +47,47 @@ public static class ChatCharacterLimit {
 
 [HarmonyPatch(typeof(ChatManager), "Awake")]
 public class ChatManager_Awake_Patch {
-    public static bool PatchSuccessful { get; private set; } = false;
-
     static void Prefix(ChatManager __instance) {
         int characterLimit = ChatCharacterLimit.CharacterLimit.Value;
         Traverse.Create(__instance).Field("characterLimit").SetValue(characterLimit);
-        Plugin.Logger.LogDebug($"Set ChatManager::characterLimit to {characterLimit}.");
-        PatchSuccessful = true;
+        Plugin.Logger.LogDebug($"ChatManager::characterLimit = {characterLimit}");
+    }
+}
+
+
+[HarmonyPatch(typeof(ChatManager), "StateActive")]
+public class ChatManager_StateActive_Patch {
+    public static bool PatchSuccessful { get; private set; } = false;
+
+    private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions) {
+        bool foundNewLineChar = false;
+        int getLengthIndex = -1;
+
+        List<CodeInstruction> codes = [.. instructions];
+        for (int i = 0; i < codes.Count; i++) {
+            if (!foundNewLineChar) {
+                if (codes[i].opcode == OpCodes.Ldstr && codes[i].operand.ToString().Equals("\n")) {
+                    foundNewLineChar = true;
+                }
+            } else if (foundNewLineChar && getLengthIndex == -1) {
+                if (codes[i].opcode == OpCodes.Callvirt && codes[i].operand.ToString().Equals("Int32 get_Length()")) {
+                    getLengthIndex = i;
+                }
+            } else if (i == getLengthIndex + 1) {
+                if (codes[i].opcode == OpCodes.Ldc_I4_S) {
+                    codes[i] = new(OpCodes.Ldc_I4, ChatCharacterLimit.CharacterLimit.Value) { labels = codes[i].labels };
+                    PatchSuccessful = true;
+                    break;
+                }
+            }
+        }
+
+        // Return the original instructions if the patch failed
+        if (!PatchSuccessful) {
+            Plugin.Logger.LogError("Failed to find the expected instructions to patch.");
+            return instructions;
+        }
+
+        return codes.AsEnumerable();
     }
 }
